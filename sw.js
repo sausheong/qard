6200c1465ab784c7006653c0682a0000e1b2b6c8
sausheong <sausheong@gmail.com>
2021-05-22 17:15:25 +0800

    tweaked service worker

--- sw.js
+++ sw.js
@@ -1,46 +1,30 @@
 const staticAssets=[
   './',
-  './static',
+  './manifest.json',
   './static/css/materialize.min.css',
   './static/img/icon-192x192.png',
   './static/img/icon-256x256.png',
   './static/img/icon-384x384.png',
   './static/img/icon-512x512.png',
-  './static/img/transparent.png',
   './static/js/jquery-3.5.1.min.js',
   './static/js/materialize.min.js',
-  './offline.html'
 ];
 
 self.addEventListener('install', async event=>{
-  const cache = await caches.open('static-cache');
-  cache.addAll(staticAssets);
+  event.waitUntil(
+    caches.open('static-cache')
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(staticAssets);
+      })
+  );
 });
 
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if(url.origin === location.url){
-      event.respondWith(cacheFirst(req));
-  } else {
-      event.respondWith(networkFirst(req));
-  }
+self.addEventListener('fetch', event => {  
+  event.respondWith(cacheFirst(event.request));
 });
 
 async function cacheFirst(req){
   const cachedResponse = caches.match(req);
   return cachedResponse || fetch(req);
-}
-
-async function networkFirst(req){
-  const cache = await caches.open('dynamic-cache');
-
-  try {
-      const res = await fetch(req);
-      cache.put(req, res.clone());
-      return res;
-  } catch (error) {
-      return await cache.match(req);
-  }
 }
